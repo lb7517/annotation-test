ba555c98cacdb5540b97cef816d82e004313be04
lb7517 <lb_987@163.com>
2020-12-10 09:16:27 +0800

    feat(annotation-test):添加输出

--- src/test/java/com/lb/annotation/AnnotationTestApplicationTests.java
+++ src/test/java/com/lb/annotation/AnnotationTestApplicationTests.java
@@ -1,13 +1,16 @@
 package com.lb.annotation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class AnnotationTestApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("1111111");
     }
 
 }
